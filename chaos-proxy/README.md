cb4e23843afed760853534da9f6c7b2791cf0e9b
Alex Chi <iskyzh@gmail.com>
2020-10-25 17:53:40 +0800

    chaos-proxy: add config utility

--- chaos-proxy/README.md
+++ chaos-proxy/README.md
@@ -4,6 +4,6 @@ Chaos proxy 是一个带延迟和限速的 HTTP 服务器。
 
 ```bash
 pip install -r requirements.txt
-python main.py start
-python main.py config latency 200
+python main.py
+python config.py latency 200
 ```
---
+++ chaos-proxy/config.py
@@ -0,0 +1,12 @@
+import sys
+import requests
+
+SERVER = "localhost:2334"
+
+if __name__ == "__main__":
+    if len(sys.argv) == 2:
+        # get
+        print(requests.get(f"http://{SERVER}/config/get/{sys.argv[1]}").content.decode())
+    if len(sys.argv) == 3:
+        # set
+        print(requests.get(f"http://{SERVER}/config/set/{sys.argv[1]}/{sys.argv[2]}").content.decode())
