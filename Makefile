4eb8dfba78dae21bc673009e45bdcd8c7d16ced2
Alex Chi <iskyzh@gmail.com>
2020-10-25 18:13:43 +0800

    ci: add GitHub Action integration (#1)
    * ci: add GitHub Action integration
    
    * ci: only run workflows when pushing to master
    
    * fix lint

---
+++ Makefile
@@ -0,0 +1,15 @@
+fix-python-%:
+	cd $* && autopep8 --in-place --recursive .
+
+lint-python-%:
+	cd $* && flake8 . --select=E9,F63,F7,F82 --show-source --statistics --exclude .venv
+	cd $* && flake8 . --max-complexity=10 --max-line-length=127 --statistics --exclude .venv
+
+lint: lint-python-blank-server lint-python-chaos-proxy
+
+test-python-%:
+	cd $* && pytest
+
+test: test-python-blank-server test-python-chaos-proxy
+
+fix: fix-python-blank-server fix-python-chaos-proxy
--- chaos-proxy/config.py
+++ chaos-proxy/config.py
@@ -6,7 +6,9 @@ SERVER = "localhost:2334"
 if __name__ == "__main__":
     if len(sys.argv) == 2:
         # get
-        print(requests.get(f"http://{SERVER}/config/get/{sys.argv[1]}").content.decode())
+        print(requests.get(
+            f"http://{SERVER}/config/get/{sys.argv[1]}").content.decode())
     if len(sys.argv) == 3:
         # set
-        print(requests.get(f"http://{SERVER}/config/set/{sys.argv[1]}/{sys.argv[2]}").content.decode())
+        print(requests.get(
+            f"http://{SERVER}/config/set/{sys.argv[1]}/{sys.argv[2]}").content.decode())
